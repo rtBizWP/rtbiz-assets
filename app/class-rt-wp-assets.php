140922cbad0bed7450652f14a66da39186864296
Dipesh Kakadiya <dipesh.kakadiya@rtcamp.com>
2014-12-02 21:22:42 +0530

    Pulgin init classes added

---
+++ app/class-rt-wp-assets.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Don't load this file directly!
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'RT_WP_Assets' ) ) {
+
+	/**
+	 * Class RT_WP_Assets
+	 * Check Dependency
+	 * Main class that initialize the rt-assets Classes.
+	 * Load Css/Js for front end
+	 *
+	 * @since  0.1
+	 *
+	 * @author dipesh
+	 */
+	class RT_WP_Assets {
+
+		/**
+		 * @var $templateURL is used to set template's root path
+		 *
+		 * @since 0.1
+		 */
+		public $templateURL;
+
+		/**
+		 * Constructor of RT_WP_Assets checks dependency and initialize all classes and set all hooks for this class
+		 *
+		 * @since 0.1
+		 */
+		public function __construct() {
+
+			if ( ! rtasset_check_plugin_dependecy() ) {
+				return false;
+			}
+
+			$this->init_globals();
+
+			add_action( 'init', array( $this, 'admin_init' ), 5 );
+			add_action( 'init', array( $this, 'init' ), 6 );
+
+			add_action( 'wp_enqueue_scripts', array( $this, 'load_scripts' ) );
+
+		}
+
+		/**
+		 * Initialize the global variables for all rtbiz-assets classes
+		 *
+		 * @since 0.1
+		 */
+		function init_globals() {
+			global $rt_asset_module, $rt_asset_device_type, $rt_asset_cpt_assets, $rt_asset_dashboard, $rt_asset_acl;
+
+			$rt_asset_module    = new RT_Asset_Module();
+			$rt_asset_device_type = new RT_Asset_Device_Type();
+			$rt_asset_cpt_assets = new RT_Asset_CPT_Assets();
+
+			$rt_asset_dashboard = new RT_Asset_Dashboard();
+			$rt_asset_acl       = new RT_Asset_ACL();
+		}
+
+		/**
+		 * Admin_init sets admin UI and functionality,
+		 * initialize the database,
+		 *
+		 * @since 0.1
+		 */
+		function admin_init() {
+			$this->templateURL = apply_filters( 'rtasset_template_url', 'rtasset/' );
+
+			global $rt_asset_admin;
+			$rt_asset_admin = new RT_Asset_Admin();
+		}
+
+
+		/**
+		 * Initialize the frontend
+		 *
+		 * @since 0.1
+		 */
+		function init() {
+
+		}
+
+		/**
+		 * Register all js
+		 *
+		 * @since 0.1
+		 */
+		function load_scripts() {
+
+			$this->localize_scripts();
+		}
+		/**
+		 * This is functions localize values for JScript
+		 * @since 0.1
+		 */
+		function localize_scripts() {
+
+			global $post;
+
+			if ( empty( $post ) ) {
+				return;
+			}
+
+			$user_edit = false;
+
+			if ( wp_script_is( 'rtasset-app-js' ) ) {
+				wp_localize_script( 'rtasset-app-js', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
+				wp_localize_script( 'rtasset-app-js', 'rtasset_post_type', get_post_type( $post->ID ) );
+				wp_localize_script( 'rtasset-app-js', 'rtasset_user_edit', array( $user_edit ) );
+			}
+
+			return true;
+		}
+	}
+}
---
+++ rtbiz-assets.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * Plugin Name: rtBiz Assets
+ * Plugin URI: http://rtcamp.com/
+ * Description: Assets System for handle & track Assets of organization
+ * Version: 0.1
+ * Author: rtCamp
+ * Author URI: http://rtcamp.com
+ * License: GPL
+ * Text Domain: rtbiz_assets
+ * Contributors: Dipesh<dipesh.kakadiya@rtcamp.com>
+ */
+
+if ( ! defined( 'RT_ASSET_VERSION' ) ) {
+	/**
+	 * Defines RT_ASSET_VERSION if it does not exits.
+	 *
+	 * @since 0.1
+	 */
+	define( 'RT_ASSET_VERSION', '0.1' );
+}
+if ( ! defined( 'RT_ASSET_TEXT_DOMAIN' ) ) {
+	/**
+	 * Defines RT_ASSET_TEXT_DOMAIN if it does not exits.
+	 *
+	 * @since 0.1
+	 */
+	define( 'RT_ASSET_TEXT_DOMAIN', 'rtbiz_assets' );
+}
+if ( ! defined( 'RT_ASSET_PATH' ) ) {
+	/**
+	 * Defines RT_ASSET_PATH if it does not exits.
+	 *
+	 * @since 0.1
+	 */
+	define( 'RT_ASSET_PATH', plugin_dir_path( __FILE__ ) );
+}
+if ( ! defined( 'RT_ASSET_URL' ) ) {
+	/**
+	 * Defines RT_ASSET_URL if it does not exits.
+	 *
+	 * @since 0.1
+	 */
+	define( 'RT_ASSET_URL', plugin_dir_url( __FILE__ ) );
+}
+if ( ! defined( 'RT_ASSET_PATH_APP' ) ) {
+	/**
+	 * Defines app folder path if it does not exits.
+	 *
+	 * @since 0.1
+	 */
+	define( 'RT_ASSET_PATH_APP', plugin_dir_path( __FILE__ ) . 'app/' );
+}
+if ( ! defined( 'RT_ASSET_PATH_ADMIN' ) ) {
+	/**
+	 *  Defines app/admin path if it does not exits.
+	 *
+	 * @since 0.1
+	 */
+	define( 'RT_ASSET_PATH_ADMIN', plugin_dir_path( __FILE__ ) . 'app/admin/' );
+}
+if ( ! defined( 'RT_ASSET_PATH_HELPER' ) ) {
+	/**
+	 *  Defines app/vendor path if it does not exits.
+	 *
+	 * @since 0.1
+	 */
+	define( 'RT_ASSET_PATH_HELPER', plugin_dir_path( __FILE__ ) . 'app/helper/' );
+}
+
+include_once RT_ASSET_PATH_HELPER . 'rtasset-functions.php';
+
+/**
+ * Using rt-lib [ RT_WP_Autoload ] class, Includes all files & external Require Libraries with in given directory.
+ *
+ * @since 0.1
+ */
+function rt_asset_include() {
+
+	//require_once RT_ASSET_PATH_VENDOR . 'redux/ReduxCore/framework.php';
+
+	global $rtast_app_autoload, $rtast_admin_autoload, $rtast_admin_metabox_autoload;
+	$rtast_app_autoload           = new RT_WP_Autoload( RT_ASSET_PATH_APP );
+	$rtast_admin_autoload         = new RT_WP_Autoload( RT_ASSET_PATH_ADMIN );
+	$rtast_admin_metabox_autoload = new RT_WP_Autoload( RT_ASSET_PATH_ADMIN . 'meta-box/' );
+
+}
+
+/**
+ * Main function that initiate rt-assets plugin
+ *
+ * @since 0.1
+ */
+function rt_asset_init() {
+
+	rt_asset_include();
+
+	global $rt_wp_ast;
+	$rt_wp_ast = new RT_WP_Assets();
+}
+add_action( 'rt_biz_init', 'rt_asset_init', 1 );
+
+/**
+ * RT_ASSET_check_dependency check for rtbiz-Assets dependency
+ * dependencies are require to run file else this plugin can't function
+ *
+ * @since 0.1
+ */
+add_action( 'init', 'rtasset_check_plugin_dependecy' );
+
+
+register_activation_hook( __FILE__, 'init_call_flush_rewrite_rules' );
+function init_call_flush_rewrite_rules(){
+	add_option( 'rtasset_flush_rewrite_rules', 'true' );
+}
